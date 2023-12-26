bab73f594a443d3add6865f9afa6ae95948b8e5d
Dikens27 <kar.karovich321@gmail.com>
2023-12-26 22:03:13 +0200

    finalCommit

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -1,18 +1,42 @@
-const FEEDBACK_MASSAGE_KEY = "feedback-form-state";
+const FEEDBACK_DATA_KEY = "feedback-form-state";
 
 const form = document.querySelector(".feedback-form");
 const textarea = form.querySelector("textarea");
 
+try {
+    const initialFormData = JSON.parse(localStorage.getItem(FEEDBACK_DATA_KEY));
 
+    Array.from(form.elements).forEach(element => {
+        const storageValue = initialFormData(element.name);
+        if (storageValue) {
+            element.value = storageValue;
+        }
+    })
+} catch (e) {
+    console.log("PARSE STORAGE ERROR");
+}
 
+const createFormObj = (formData) => {
+    const formObj = {};
 
+    formData.forEach((value, key) => {
+        formObj[key] = value;
+    });
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
+    return formObj;
+}; 
+
+form.addEventListener("input", () => {
+    const formData = new FormData(form);
+
+    localStorage.setItem(FEEDBACK_DATA_KEY, JSON.stringify(createFormObj(formData)));
 });
 
-textarea.addEventListener("input", (event) => {
-    const massage = event.currentTarget.value;
+form.addEventListener("submit", (event) => {
+    event.preventDefault(); 
+    localStorage.removeItem(FEEDBACK_DATA_KEY);
+    const formData = new FormData(form);
+    console.log(createFormObj(formData));
+    form.reset();
+});
 
-    localStorage.setItem(FEEDBACK_MASSAGE_KEY, massage);
-})
