3e2090f986383b5ceb2432a481b3aac3c9bdecb5
Dikens27 <kar.karovich321@gmail.com>
2024-01-04 11:25:51 +0200

    finalochka

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,19 +1,20 @@
 const FEEDBACK_DATA_KEY = "feedback-form-state";
-
 const form = document.querySelector(".feedback-form");
+const initialFormData = JSON.parse(localStorage.getItem(FEEDBACK_DATA_KEY));
 
-try {
-    const initialFormData = JSON.parse(localStorage.getItem(FEEDBACK_DATA_KEY));
-
-    Array.from(form.elements).forEach(element => {
-        const storageValue = initialFormData[element.name];
-        if (storageValue) {
-            element.value = storageValue;
+if (initialFormData) {
+    try {
+        if (initialFormData["email"]) {
+            form.elements["email"].value = initialFormData["email"];
         }
-    })
-} catch (e) {
-    console.error("Помилка парсингу даних локального сховища");
+        if (initialFormData["message"]) {
+            form.elements["message"].value = initialFormData["message"];
+        }
+    } catch (e) {
+        console.error("Помилка парсингу даних локального сховища");
+    }
 }
+
 
 const createFormObj = (formData) => {
     const formObj = {};
@@ -34,11 +35,22 @@ form.addEventListener("input", () => {
 form.addEventListener("submit", (event) => {
     event.preventDefault(); 
     localStorage.removeItem(FEEDBACK_DATA_KEY);
-    // if (event.currentTarget.value) {
-    //     window.alert("Поле не може бути пустим")
-    // }
+
+    const data = new FormData(event.target);
+    let fill = true;
+    
+    data.forEach((value) => {
+        if (!value) {
+            fill = false;
+            return;
+        }
+    });
+    if (fill === false) {
+        window.alert("Поле не може бути пустим")
+    }
     const formData = new FormData(event.currentTarget);
     console.log(createFormObj(formData));
     event.currentTarget.reset();
 });
 
+ 
